ad8a87047d38d5472a90bf1f1b664fb7c63a9311
Mohamed Abdi <khadar.abdi165@gmail.com>
2025-04-08 19:16:42 +0200

    domain entities added and made user class

--- FinApp.Domain/Entities/User.cs
+++ FinApp.Domain/Entities/User.cs
@@ -8,5 +8,13 @@ namespace FinApp.Domain.Entities
 {
     public class User
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Settings Settings { get; set; }
+        public List<Account> Accounts { get; set; } = new List<Account>();
+        public List<Budget> Budgets { get; set; } = new List<Budget>();
+        public List<Report> Reports { get; set; }
     }
 }
